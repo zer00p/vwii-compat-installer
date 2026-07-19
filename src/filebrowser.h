a2203fdf9102ad4d7e9e88e5b9da5d33bb30e5e0
zer00p <zer00p@users.noreply.github.com>
2026-07-19 11:34:32 +0200

    adjust copyright header

--- src/filebrowser.h
+++ src/filebrowser.h
@@ -1,5 +1,5 @@
-/* WAD Installer integration
- *   Copyright (C) 2026
+/* Compat Title Installer
+ *   Copyright (C) 2026  zer00p
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
